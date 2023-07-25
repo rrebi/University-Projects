b7c13a3a98c7ccf982671e1d0d0c59ade08bb732
rrebi <97788460+rrebi@users.noreply.github.com>
2023-07-25 15:37:13 +0300

    Delete Semester 4/Systems for Design and Implementation/lab-5 directory

--- Semester 4/Systems for Design and Implementation/lab-5/lab1/lab1/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Books, BookSold, Publisher, Customers
-
-admin.site.register(Books)
-admin.site.register(Publisher)
-admin.site.register(Customers)
-admin.site.register(BookSold)
