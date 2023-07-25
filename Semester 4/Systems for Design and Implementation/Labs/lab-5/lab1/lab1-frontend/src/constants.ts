8588d5bc82eaf51591a57db378424a238134a20b
rrebi <97788460+rrebi@users.noreply.github.com>
2023-07-25 15:49:10 +0300

    Delete Semester 4/Systems for Design and Implementation/Labs/lab-5/lab1/lab1-frontend directory

--- Semester 4/Systems for Design and Implementation/Labs/lab-5/lab1/lab1-frontend/src/constants.ts
+++
@@ -1,6 +0,0 @@
-
-const PROD_BACKEND_API_URL = "";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000";
-
-export const GlobalURL =
-	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
